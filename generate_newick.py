7e166358b23eb9a398960da0ae9db876d6beffd3
Anagha <anaghakrishna92@gmail.com>
2024-01-06 14:59:31 -0500

    changed to dissimilarity scores and fixed the tree lengths

--- generate_newick.py
+++ generate_newick.py
@@ -45,7 +45,7 @@ def call_ispcr() -> dict[set]:
         amplicon_dict[assembly_file[:assembly_file.index(".")]] = list({seq for seq in amplicon_list[1::2]})
         #set does not preserve the order of the amplicons, so each time NW is run, it gives different score
         #trying with list
-        amplicon_dict[assembly_file[:assembly_file.index(".")]] = [seq for seq in amplicon_list[1::2]]
+        # amplicon_dict[assembly_file[:assembly_file.index(".")]] = [seq for seq in amplicon_list[1::2]]
         amplicon_list.clear() #empty the list before next iteration
 
     #uncomment to print amplicons for each organism
@@ -57,7 +57,7 @@ def call_ispcr() -> dict[set]:
 def generate_sim_matrix(amplicon_dict: dict[set]) -> tuple[list[list[int]], list[str]]:
     #TD:add decorator to deal with multiple amplicons for same organism
     #Initialize similarity matrix with 0s
-    sim_matrix = [[0 for _ in range(len(amplicon_dict))] for _ in range(len(amplicon_dict))]
+    sim_matrix = [[1000 for _ in range(len(amplicon_dict))] for _ in range(len(amplicon_dict))]
     org_list = list(amplicon_dict.keys())
     # print(type(amplicon_dict.keys()))
     #align every organism's amplicon with every other to get the similarity score and fill the matrix
@@ -72,14 +72,14 @@ def generate_sim_matrix(amplicon_dict: dict[set]) -> tuple[list[list[int]], list
                 seq2 = amplicon_dict[org2][0]
                 # print(f"{org1}: {seq1}")
                 # print(f"{org2}: {seq2}")
-                aln1, score1 = upgma.needleman_wunsch(seq1, seq2, match, mismatch, gap)
-                aln2, score2 = upgma.needleman_wunsch(seq1, reverse_complement(seq2), match, mismatch, gap)
+                _, score1 = upgma.needleman_wunsch(seq1, seq2, match, mismatch, gap)
+                _, score2 = upgma.needleman_wunsch(seq1, reverse_complement(seq2), match, mismatch, gap)
                 # print(score)
                 score = score1 if score1 > score2 else score2
                 # sim_matrix[ind1][ind2] = score
                 #normalize the scores
-                sim_matrix[ind1][ind2] = round(score / max(len(seq1), len(seq2)), 3)
-                # sim_matrix[ind1][ind2] = max(len(seq1), len(seq2))*match - score
+                # sim_matrix[ind1][ind2] = round(score / max(len(seq1), len(seq2)), 3)
+                sim_matrix[ind1][ind2] = max(len(seq1), len(seq2))*match - score
                 # break
         # break
     # for i in sim_matrix:
@@ -101,20 +101,22 @@ def finding_clusters(sim_matrix: list[list[float]], org_list: list[str]):
     # sim_matrix = [[0.0, 1.48, 1.58, 0.0, 0.03, 0.02, 1.59, 0.72, 0.08, 0.09], [1.48, 0.0, 1.53, 0.11, 0.13, 0.17, 1.3, 0.72, 0.14, 0.14], [1.58, 1.53, 0.0, -0.06, 0.08, 0.08, 1.58, 0.83, 0.11, 0.04], [0.0, 0.11, -0.06, 0.0, 0.77, 0.91, -0.19, -0.06, 0.68, 1.56], [0.03, 0.13, 0.08, 0.77, 0.0, 2.16, 0.04, 0.19, 1.28, 0.75], [0.02, 0.17, 0.08, 0.91, 2.16, 0.0, 0.04, 0.16, 1.48, 0.84], [1.59, 1.3, 1.58, -0.19, 0.04, 0.04, 0.0, 0.89, 0.12, 0.01], [0.72, 0.72, 0.83, -0.06, 0.19, 0.16, 0.89, 0.0, 0.14, -0.04], [0.08, 0.14, 0.11, 0.68, 1.28, 1.48, 0.12, 0.14, 0.0, 0.8], [0.09, 0.14, 0.04, 1.56, 0.75, 0.84, 0.01, -0.04, 0.8, 0.0]]
     # org_list = ["Pseudomonas_aeruginosa_UCBPP-PA14", "Mycobacterium_tuberculosis_H37Rv", "Staphylococcus_aureus_NCTC_8325", "Sulfolobus_islandicus_M", "Vibrio_cholerae_N16961", "Escherichia_coli_K12", "Wolbachia", "Ferroplasma_acidiphilum_Y", "Treponema_phagedenis_strain_B43", "Nitrososphaera_viennensis_EN76"]
     np_sim_matrix = np.array(sim_matrix)
-    # for i in sim_matrix:
-    #     print(i)
+    org_dist = {}
+    for i in sim_matrix:
+        print(i)
     # print(max_ind[0])
     # print(np_sim_matrix[max_ind[0,0],max_ind[0,1]])
     while len(np_sim_matrix) > 1:
         # print(np_sim_matrix)
-        max_ind_pair = np.argwhere(np_sim_matrix == np_sim_matrix.max())[0]
+        max_ind_pair = np.argwhere(np_sim_matrix == np_sim_matrix.min())[0]
         max_value = np_sim_matrix[max_ind_pair[0]][max_ind_pair[1]]
         modified_sim = [] #np.empty(len(np_sim_matrix)-1)
         # print(max_indices)
         # print(max_value)
         # for the row and col containing max value, calculate the average for every other row and col
         # for max_ind_pair in max_indices[::2]:
-        max_avg = max_value / 2
+        org1_avg = max_value / 2
+        org2_avg = max_value / 2
         # print(max_ind_pair)
         for i in range(0, len(np_sim_matrix)):
             if i == max_ind_pair[0] or i == max_ind_pair[1]:
@@ -127,7 +129,7 @@ def finding_clusters(sim_matrix: list[list[float]], org_list: list[str]):
     #     for col in range(len(np_sim_matrix)):
     # reduced_sim_matrix = np.zeros((len(np_sim_matrix)-1, len(np_sim_matrix)-1))
         np_modified_sim = np.array(modified_sim)
-        np_modified_sim = np.insert(np_modified_sim, max_ind_pair[0], 0.0)
+        np_modified_sim = np.insert(np_modified_sim, max_ind_pair[0], 1000.0)
         # print(np_modified_sim)
         
         # np_mod_sim = np.insert(np_modified_sim, max_ind_pair[0], 0.0)
@@ -147,13 +149,19 @@ def finding_clusters(sim_matrix: list[list[float]], org_list: list[str]):
         np_sim_matrix[:, row_col_to_del[0]] = np_modified_sim.T[:]
         # np_sim_matrix = np.insert(np_sim_matrix, row_col_to_del[0], np_modified_sim[:], axis = 0)
         # np_sim_matrix = np.insert(np_sim_matrix.T, row_col_to_del[0], np_modified_sim[:], axis = 0)
+        if org_list[max_ind_pair[0]] in org_dist:
+            org1_avg = (max_value / 2) - org_dist[org_list[max_ind_pair[0]]]
+        if org_list[max_ind_pair[1]] in org_dist:
+            org2_avg = (max_value / 2) - org_dist[org_list[max_ind_pair[1]]]
 
 
         # np_sim_matrix[:, row_col_to_del[0]:row_col_to_del[1] + 1] = np_modified_sim[:-1, np.newaxis]
         # print(np_sim_matrix)
 
+
         #generate newick string
-        org_list[max_ind_pair[0]] = '(' + org_list[max_ind_pair[0]] + ":" + str(max_avg) + "," + org_list[max_ind_pair[1]] + ":" + str(max_avg) + ')'
+        org_list[max_ind_pair[0]] = '(' + org_list[max_ind_pair[0]] + ":" + str(org1_avg) + "," + org_list[max_ind_pair[1]] + ":" + str(org2_avg) + ')'
+        org_dist[org_list[max_ind_pair[0]]] = max_value / 2
         del org_list[max_ind_pair[1]]
     print(org_list)
         
